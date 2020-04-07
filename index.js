c39d2fed08089abdb5af424aa7e6105a634bbe38
Duke-Pereira <ald59@tmw.com>
2020-04-07 16:21:49 -0700

    Backend ready for tweeting

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const router = express.Router()
 const path = require('path')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
-
-require('dotenv').config()
+const Twit = require('twit')
+const client = new Twit({
+  consumer_key: 'YKROgK9sAHpO0cWgukYbza1rl',
+  consumer_secret: 'PnUfmtBfjIQUW76ODq3DjESz7fYmw7kTcPvAsPJAgCSt6UV3yX',
+  access_token: '1137103960500609024-vaT8SzBbXT0XgyvObtH5F9YRwVnZ54',
+  access_token_secret: '1S25Lh3ABWpVUgOGtL6F1Q7jZ5IDPXpWBLRJdz59w6ycn'
+})
 
 const app = express()
 const port = process.env.PORT || '3001'
@@ -19,8 +25,21 @@ app.get('/', (req, res) => {
   res.render('index', { title: 'SozoPro' })
 })
 
-const tweetRouter = require('./routes/tweet')
-app.use('/tweet', tweetRouter)
+
+app.get('/auth', (req, res) => {
+  client.get('account/verify_credentials')
+    .then(user => res.send(user))
+    .catch(err => {
+      console.log(err)
+      res.send(err)
+    })
+})
+
+app.post('/new', (req, res) => {
+  client.post('statuses/update', { status: `${req.body.status}` }, (err, data, response) => {
+    err ? console.log(err) : res.send('Tweeted!')
+  })
+})
 
 // Force https in production
 if (app.get('env') === 'production') {
